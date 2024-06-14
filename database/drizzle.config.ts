f0bbbd9e7d2d90daa90d475378d8a52ce4026c04
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-06-14 18:43:21 +0200

    drizzle db scaffolding

--- database/drizzle.config.ts
+++ database/drizzle.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         password: process.env.DB_PASSWORD ?? "",
         host: process.env.DB_HOST ?? "localhost",
         database: process.env.DB_NAME ?? "postgres",
-        ssl: true
+        ssl: false
     },
     verbose: true,
     strict: true,
