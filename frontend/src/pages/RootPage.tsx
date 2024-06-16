67d1889737684be31f615f508e961d99affd13d5
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-06-16 21:46:00 +0200

    a lot of logic for auth redirect handling

--- frontend/src/pages/RootPage.tsx
+++ frontend/src/pages/RootPage.tsx
@@ -1,8 +1,50 @@
 import Navbar from "@components/Navbar"
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation, useNavigate } from "react-router-dom"
 import { Toaster } from "@/components/ui/toaster"
+import { useContext, useEffect } from "react"
+import authContext from "@/utils/authContext"
+import { useToast } from "@/components/ui/use-toast"
 
 export default () => {
+
+    const auth = useContext(authContext);
+    const navigate = useNavigate();
+    const path = useLocation()
+    const { toast } = useToast();
+    useEffect(() => {
+        if (path.hash != "") {
+            const params = new URLSearchParams(path.hash.substring(1))
+            var data: any = {};
+            console.warn(params.get("type"))
+            for (const [key, val] of params) {
+                data[key] = val;
+            }
+
+            if (data.access_token) auth.createUser(data, true).then(
+                u => console.log(u)
+            );
+
+            if (data.message) {
+                toast({
+                    title: data.message
+                })
+            }
+            switch (params.get("type")) {
+                case "recovery": {
+                    navigate('new-password')
+                }
+                case "email_change": {
+                    navigate("/profile")
+                }
+                default: {
+                    navigate("/")
+                }
+            }
+        }
+
+    }, [location])
+
+
     return (
         <div className="w-full h-full flex flex-col">
             <Navbar />
