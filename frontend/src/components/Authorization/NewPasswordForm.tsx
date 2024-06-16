7e02a5e9a29291bad988659082c019e7b49a116b
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-06-16 22:11:53 +0200

    password resets working

--- frontend/src/components/Authorization/NewPasswordForm.tsx
+++ frontend/src/components/Authorization/NewPasswordForm.tsx
@@ -1,5 +1,64 @@
-export default () => {
+import {
+    Card,
+    CardContent as Content,
+    CardDescription as Description,
+    CardFooter as Footer,
+    CardHeader as Header,
+    CardTitle as Title,
+} from "@components/ui/card"
+import { Input } from "@components/ui/input"
+import { Switch } from "@components/ui/switch"
+import { Label } from "@components/ui/label"
+import { Button } from "@components/ui/button";
+import { useState } from 'react'
+import { Skeleton } from "@components/ui/skeleton";
+
+export interface NewPasswordData {
+    password: string,
+    repeatPassword: string
+}
+
+export interface NewPasswordErrors {
+    password: string,
+    repeatPassword: string
+}
+
+
+interface Props {
+    validate: (data: NewPasswordData) => NewPasswordErrors | null,
+    onSubmit: (data: NewPasswordData) => void
+}
+
+export default ({ validate, onSubmit }: Props) => {
+
+    const [errors, setErrors] = useState<NewPasswordErrors | null>(null);
+    const [data, setData] = useState<NewPasswordData>({ password: "", repeatPassword: "" });
+
+    const submit = () => {
+        const errors = validate(data);
+        if (errors == null) {
+            setErrors(null)
+            onSubmit(data);
+        } else {
+            setErrors(errors)
+        }
+
+    }
+
     return (
-        <> Reset Password </>
+        <Card className="max-w-[400px] max-h-[600px]">
+            <Header>
+                <Title> Create a new password </Title>
+            </Header>
+            <Content className="p-2 flex flex-col gap-2">
+                <Input type="password" onChange={(e) => setData({ ...data, password: e.target.value })} placeholder="Password" />
+                <p>{errors?.password}</p>
+                <Input type="password" onChange={(e) => setData({ ...data, repeatPassword: e.target.value })} placeholder="Password confirmation" />
+                <p>{errors?.repeatPassword}</p>
+            </Content>
+            <Footer className="flex flex-col gap">
+                <Button onClick={submit} > Change password </Button>
+            </Footer>
+        </Card>
     )
 }
--- frontend/src/pages/NewPasswordPage.tsx
+++ frontend/src/pages/NewPasswordPage.tsx
@@ -1,7 +1,56 @@
-import NewPasswordForm from "@/components/Authorization/NewPasswordForm"
+import NewPasswordForm, { NewPasswordData } from "@/components/Authorization/NewPasswordForm"
+import { useToast } from "@/components/ui/use-toast";
+import authContext from "@/utils/authContext"
+import { AxiosError } from "axios";
+import { useContext } from "react"
+import { useNavigate } from "react-router-dom";
 
 export default () => {
+
+    const auth = useContext(authContext);
+    const navigate = useNavigate()
+    const { toast } = useToast()
+
+    const validate = (data: NewPasswordData) => {
+        const errors: any = {}
+        // Password validation
+        const passwordRegex = /^(?=.*[!@#$%^&*(),.?":{}|<>_])(?=.*\d)[A-Za-z\d!@#$%^&*(),.?":{}|<>_]{8,}$/
+        if (!passwordRegex.test(data.password)) {
+            errors.passwordError = "Invalid password"
+        }
+
+        // Password and confirmation match
+        if (data.password !== data.repeatPassword) {
+            errors.passwordConfirmationError = "Passwords do not match"
+        }// Return errors if any
+        if (errors.emailError || errors.passwordError || errors.passwordConfirmationError || errors.usernameError) {
+            return errors
+        }
+
+        return null
+
+    }
+    const setPassword = async (data: NewPasswordData) => {
+        try {
+            const user = auth.currentUser()!;
+            const res = await user.update({ password: data.password })
+            console.log(res)
+            toast({
+                title: "Password change succesful",
+                description: "please login again"
+            });
+            await user.logout();
+            navigate("/login")
+        } catch (error: any) {
+            toast({
+                title: "Password update unsuccesful",
+                description: error.json.msg,
+                variant: 'destructive'
+            })
+        }
+    }
+
     return (
-        <NewPasswordForm />
+        <NewPasswordForm validate={validate} onSubmit={setPassword} />
     )
 }
