02b2d9ebe428c22e549c34153c0e995dd9f47ac4
Taras Zhyhulin <zhyhulin@student.agh.edu.pl>
2024-06-19 11:31:44 +0200

    added home page content

--- frontend/src/pages/RootPage.tsx
+++ frontend/src/pages/RootPage.tsx
@@ -1,54 +1,31 @@
 import Navbar from "@components/Navbar"
-import { Outlet, useLocation, useNavigate } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 import { Toaster } from "@/components/ui/toaster"
-import { useContext, useEffect } from "react"
-import authContext from "@/utils/authContext"
-import { useToast } from "@/components/ui/use-toast"
 
 export default () => {
-
-    const auth = useContext(authContext);
-    const navigate = useNavigate();
-    const path = useLocation()
-    const { toast } = useToast();
-    useEffect(() => {
-        if (path.hash != "") {
-            const params = new URLSearchParams(path.hash.substring(1))
-            var data: any = {};
-            for (const [key, val] of params) {
-                data[key] = val;
-            }
-
-            if (data.access_token) auth.createUser(data, true);
-
-            if (data.message) {
-                toast({
-                    title: data.message
-                })
-            }
-            console.warn(params.get("type"))
-            switch (params.get("type")) {
-                case "recovery": {
-                    navigate("/new-password")
-                    break
-                }
-                case "email_change": {
-                    navigate("/profile")
-                    break
-                }
-                default: {
-                    navigate("/")
-                }
-            }
-        }
-
-    }, [location])
-
-
     return (
         <div className="w-full h-full flex flex-col">
             <Navbar />
             <Outlet />
+            <div className="mt-6">
+                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                    Welcome to the Calendar App!
+                </h1>
+                <p className="leading-7 [&:not(:first-child)]:mt-6">
+                    This application allows you to manage your time effectively by introducing your plans into the calendar.
+                </p>
+                <ul className="my-6 ml-6 list-disc [&>li]:mt-2 mb-10">
+                    <li>All your events are visible in the calendar, so can be tracked conveniently</li>
+                    <li>One or multiple events can be exported from the file with <i>ical</i> format</li>
+                    <li>One or multiple events may be imported from the file with <i>ical</i> format</li>
+                    <li>Your account info and events in the calendar are secure and available only to very you</li>
+                    <li>If you don't have an account, go to Account-&gt;Sign up</li>
+                    <li>If you have already registered, welcome back! Go to Account-&gt;Log in and provide your credentials</li>
+                    <li>You can reset your password in case you forgot it</li>
+                </ul>
+                <p>Here you can find technical documentation and the code: <br/>
+                <a href="https://github.com/TypicalUsername-ai/calendar-app">Github</a></p>
+            </div>
             <Toaster />
         </div>
     )
